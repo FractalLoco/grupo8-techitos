1cadce03f705a76ff861815f28d0c08a3bad3ada
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-07-08 23:38:20 -0400

    FIX: Se arregló un error de semántica

--- techo-frontend/frontend/src/services/apiBase.js
+++ techo-frontend/frontend/src/services/apiBase.js
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 
-
-export const API_BASE = import.meta?.env?.VITE_URL_BACKEND;
+export const API_BASE = import.meta.env.VITE_URL_BACKEND || import.meta.env.URL_BACKEND;
 
 if (!API_BASE) {
     throw new Error("API no proporcionada");
