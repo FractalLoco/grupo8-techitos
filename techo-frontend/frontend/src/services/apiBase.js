62566782eb0343c15fad0106c1bd9e1a1e9be5ba
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-07-08 23:15:22 -0400

    FIX: Se reinstaló dotenv

--- techo-frontend/frontend/src/services/apiBase.js
+++ techo-frontend/frontend/src/services/apiBase.js
@@ -1,4 +1,6 @@
-export const API_BASE = import.meta?.env?.VITE_URL_BACKEND || process?.env?.VITE_URL_BACKEND;
+
+
+export const API_BASE = import.meta?.env?.VITE_URL_BACKEND;
 
 if (!API_BASE) {
     throw new Error("API no proporcionada");
--- techo-frontend/frontend/vite.config.js
+++ techo-frontend/frontend/vite.config.js
@@ -2,12 +2,16 @@
 import { defineConfig } from 'vite';
 // Importo el plugin oficial de React que habilita JSX y Fast Refresh en desarrollo
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.VITE_URL_BACKEND": JSON.stringify(process.env.VITE_URL_BACKEND),
+  },
   optimizeDeps: {
     // Pre-bundleo las dependencias más pesadas al arrancar para reducir el tiempo de carga en desarrollo
-    include: ['react', 'react-dom', 'react-router-dom'],
+    include: ['react', 'react-dom', 'react-router-dom', 'dotenv'],
   },
   server: {
     host: '0.0.0.0',
