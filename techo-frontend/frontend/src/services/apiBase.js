fcba8eda2e90c7d2e739f0c90db69a5e0c599157
Rodriu12 <rodrigo.alarcon2301@alumnos.ubiobio.cl>
2026-07-08 23:03:57 -0400

    FIX: Se cambió un import incorrecto

--- techo-frontend/frontend/src/services/apiBase.js
+++ techo-frontend/frontend/src/services/apiBase.js
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-
-export const API_BASE = process?.env?.VITE_URL_BACKEND || "http://localhost:3000";
+export const API_BASE = import.meta?.env?.VITE_URL_BACKEND || "http://localhost:3000";
 
 export default API_BASE;
